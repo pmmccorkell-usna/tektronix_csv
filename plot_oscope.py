2f6c6fe69986e5210c58adfaef89031901beca20
Patrick <35158605+pmmccorkell@users.noreply.github.com>
2021-05-05 08:34:28 -0400

    Update plot_oscope.py

--- plot_oscope.py
+++ plot_oscope.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
-# import matplotlib.cbook as cbook
 import pandas as pd
 from os import listdir
 
-# Path of the working directory
+# Absolute path of the working directory
 working_directory = '/media/pi/disk/'
 
 # Test filename for testing purposes.
 #filename = 'TEK00000.CSV'
 
+# Function to find csv files.
 def find_files():
 	file_set = set()
 	for filename in listdir(working_directory):
 		
 		# Add the filename to set() if *.csv extension.
-		# Adds a null entry of 'working_directory' that needs to be discarded later.
+		# Side effect: adds a null entry of 'working_directory' that needs to be discarded later.
 		file_set.add(working_directory + filename.lower().endswith('csv') * filename)
 		
 	# Discard the null entry.
@@ -23,19 +23,34 @@ def find_files():
 	# return the set of csv files
 	return file_set
 
+# Function to parse the csv files into Pandas Data Frames.
 def read_csv():
+	# pull the csv files as a set()
 	csv_files = find_files()
+
+	# setup some variables
 	pd_dict = {}
 	i = 0
+
+	# iterate through each file in the set()
 	for csv_file in csv_files:
-		pd_dict[i] = pd.read_csv(csv_file,names = ['x','y'])
+		# Use Pandas to read each csv file and store the Data Frame in an iterated dictionary
+		pd_dict[i] = pd.read_csv(csv_file,names = ['time','voltage'])
+		# iterate the dictionary counter
 		i += 1
 	return pd_dict
 
+# Function to plot the Pandas Data Frames in the same manner as an Oscope.
 def plot_csv():
+	# Pull the Pandas Data Frames as a dictionary{}
 	data_frames = read_csv()
-	for i in data_frames:
-		data_frames[i].plot(x='x',y='y')
+
+	# Iterate through the dictionary{} of Data Frames.
+	for k in data_frames:
+		# Plot each Data Frame separately. Assigning the x-axis and y-axis as it is on the Oscope.
+		data_frames[k].plot(x='time',y='voltage')
+	
+	# Display all of the plots.
 	plt.show()
 
 plot_csv()
